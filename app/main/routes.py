e4bf79e00144d692890ed2bfe70e8273e7d5e2f2
J8r41 <kalashnikovroman41@yandex.ru>
2023-04-12 15:48:12 +0300

    added search field

--- app/main/routes.py
+++ app/main/routes.py
@@ -5,7 +5,7 @@ from flask import render_template, request
 from app import db
 from app.main import bp
 from app.main.models import Adress, Resident
-
+from sqlalchemy import or_
 
 fake = Faker("ru-RU")
 
@@ -14,8 +14,24 @@ fake = Faker("ru-RU")
 def index():
     page = request.args.get("page", 1, type=int)
     per_page = 20
-    data = Adress.query.paginate(
-        page=page, per_page=per_page, error_out=False
+    q = request.args.get("search", "")
+    data = (
+        Adress.query.join(Resident, Resident.id == Adress.id)
+        .filter(
+            or_(
+                Adress.country.like(f"%{q}%"),
+                Adress.subject.like(f"%{q}%"),
+                Adress.city.like(f"%{q}%"),
+                Adress.index.like(f"%{q}%"),
+                Adress.street.like(f"%{q}%"),
+                Adress.number_building.like(f"%{q}%"),
+                Adress.number_flat.like(f"%{q}%"),
+                Resident.second_name.like(f"%{q}%"),
+                Resident.first_name.like(f"%{q}%"),
+                Resident.middle_name.like(f"%{q}%"),
+            )
+        )
+        .paginate(page=page, per_page=per_page, error_out=False)
     )
     pages = data.iter_pages(
         left_edge=2, left_current=2, right_current=5, right_edge=2
