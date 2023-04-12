90ebc351c2ec315a6b34e2f330065a9e85ae2abe
J8r41 <kalashnikovroman41@yandex.ru>
2023-04-12 12:33:18 +0300

    added test recort to db

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,7 +1,9 @@
 from app.main import bp
 from flask import render_template
+from app.main.models import Adress
 
 
 @bp.route("/")
 def index():
-    return render_template("main/index.html", title='index')
+    data = Adress.query.all()
+    return render_template("main/index.html", title="index", data=data)
